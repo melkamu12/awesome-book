c76c29d6b974624ecb03227438edebfa331c17a1
Melkamu Almawu Ayen <malmawu@gmail.com>
2023-06-12 22:15:28 +0300

    Fix arrow function and MIT error

--- modules/setup.js
+++ modules/setup.js
@@ -1,17 +1,17 @@
 const setup = () => {
-  const header = document.querySelector(".content");
+  const header = document.querySelector('.content');
   const stickyHeader = header.offsetTop;
 
   window.onscroll = () => {
     if (window.scrollY > stickyHeader) {
-      header.classList.add("sticky");
+      header.classList.add('sticky');
     } else {
-      header.classList.remove("sticky");
+      header.classList.remove('sticky');
     }
   };
 
-  if (document.location.hash === "" || document.location.hash === "#") {
-    document.location.hash = "#books";
+  if (document.location.hash === '' || document.location.hash === '#') {
+    document.location.hash = '#books';
   }
 };
 
